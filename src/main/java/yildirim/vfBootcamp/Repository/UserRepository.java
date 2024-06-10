ef02101839b5af5ef1e25549d0ec8ee5b54994e6
Mustafa <72528911+yildirim7mustafa@users.noreply.github.com>
2024-06-11 00:39:19 +0300

    Update UserRepository.java

--- src/main/java/yildirim/vfBootcamp/Repository/UserRepository.java
+++ src/main/java/yildirim/vfBootcamp/Repository/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import yildirim.vfBootcamp.Entity.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
-  // selam
+
 }
